d5f0dc939c64948bbf6c81ba8069b0bc101ac7cf
Shubham <shubhamlahase@hmail.com>
2023-02-23 17:49:45 +0530

    totalRecovered routes done

--- src/index.js
+++ src/index.js
@@ -8,6 +8,25 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 const { connection } = require('./connector')
 
+app.get("/", (req,res) => {
+    res.send("Tested OK")
+})
+
+app.get("/totalRecovered", async (req,res) => {
+    try{
+        const result = await connection.aggregate([
+            { $group : {_id:"total", "recovered":{$sum:"$recovered"}} }
+        ])
+        res.status(200).json({
+            data: result[0]
+        })
+    } catch(err) {
+        res.status(500).json({
+            status: "Failed",
+            message: err.message
+        })
+    }
+})
 
 
 
